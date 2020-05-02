43dcb1a7d5521a973ed325086673be822e468c44
Emily Carlsen <emilytcarlsen@gmail.com>
2020-05-02 00:01:41 -0400

    Add basic socket io user detection

--- server/server.js
+++ server/server.js
@@ -1,1 +1,14 @@
-console.log('test')
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
