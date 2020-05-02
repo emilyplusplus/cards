882555dbd95e365c4048149109c482eaaf6c7c2f
Emily Carlsen <emilytcyt@gmail.com>
2020-05-02 17:33:59 -0400

    Add nicer looking playing card

--- src/components/CardTest.vue
+++ src/components/CardTest.vue
@@ -11,17 +11,13 @@
       @warp="handleWarp"
       @pinch="handlePinch"
     >
-      <v-card
-        class="mx-auto"
-        max-width="344"
-        outlined
-      >
-        <v-list-item three-line>
-          <v-list-item-content>
-            <v-list-item-title class="headline mb-1">K ♠</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-card>
+      <div class="card">
+        <h1>K<i class="fas fa-heart"></i></h1>
+        <div class="spacer">
+
+        </div>
+        <h1 class="bottom">K<i class="fas fa-heart"></i></h1>
+      </div>
     </Moveable>
   </v-container>
 </template>
@@ -91,5 +87,47 @@
 <style scoped>
 .mx-auto {
   margin: 0;
+}
+
+.card {
+  border-radius: 5px;
+  width: 200px;
+  height: 300px;
+  box-shadow: 0 0 8px 0 #ccc;
+  overflow: hidden;
+  position: relative;
+}
+
+h1 {
+  position: absolute;
+  left: 20px;
+  top: 20px;
+  margin: 0;
+  font-family: 'Montserrat', Arial, Helvetica, sans-serif;
+  color: rgb(180, 23, 31);
+}
+
+h1 i {
+  font-size: 0.85em;
+  margin-left: 5px;
+}
+
+h1.bottom {
+  bottom: 20px;
+  right: 20px;
+  transform: rotate(180deg);
+}
+
+.card .spacer {
+  position: absolute;
+  top: 75px;
+  left: -50px;
+  height: 150px;
+  width: 300px;
+  background: url('https://images.unsplash.com/photo-1538113300105-e51e4560b4aa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=center&w=300&q=60');
+  background-position-y: -185px;
+  transform: rotate(-5deg);
+  border-top: 1px solid #666;
+  border-bottom: 1px solid #666;
 }
 </style>
